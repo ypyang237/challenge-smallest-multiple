ef16095576ff7e0ce086eb3ed5b64645a61d00c2
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-18 20:12:08 -1000

    completed

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,30 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
+module.exports = function(ceiling) {
   // do work here
 
+  var number = 1;
+  var myArray = [];
 
-  return 0;
+  for (i = 1; i < ceiling; i++) {
+    myArray.push(i);
+  }
+
+  function check(number) {
+    for (var k in myArray) {
+      if(number % myArray[k] !== 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  while(check(number) === false) {
+    number ++;
+  }
+
+  return number;
+
 };
+
